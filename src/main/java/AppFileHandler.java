802c4c3044f1e43534bba78b0006d6b19ae2961a
Dean Lynch <dean.lynch49@mail.dcu.ie>
2019-01-02 12:50:00 +0000

    selected file path now overrides previous file

--- src/main/java/AppFileHandler.java
+++ src/main/java/AppFileHandler.java
@@ -1,5 +1,7 @@
+import javafx.application.Platform;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,8 +17,8 @@ public class AppFileHandler extends AppDefault {
 
     @Override
     public void addUIControls(GridPane gridPane, Stage primaryStage) {
-        Label headerLabel = new Label("Welcome");
-        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        Label headerLabel = new Label("Please select a .KDBX file");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
         gridPane.add(headerLabel, 0, 0, 2, 1);
         GridPane.setHalignment(headerLabel, HPos.CENTER);
         GridPane.setMargin(headerLabel, new Insets(20, 0, 20, 0));
@@ -36,22 +38,26 @@ public class AppFileHandler extends AppDefault {
         Button fileButton = new Button("Select File");
         gridPane.add(fileButton, 1, 1);
 
-        fileButton.setOnAction(event -> {
-            File file = fileChooser.showOpenDialog(primaryStage);
-
-            if (file != null) {
-                for (Label child : gridPane.getChildren())
-                gridPane.getChildren().remove(selectedFileLabel);
-                Label newSelectedFileLabel = new Label(file.getAbsolutePath());
-                newSelectedFileLabel.setStyle("-fx-font-weight: bold");
-                gridPane.add(newSelectedFileLabel, 1, 2);
-            }
-        });
-
         Button openButton = new Button("Open File");
         gridPane.add(openButton, 0, 3, 2, 1);
         GridPane.setHalignment(openButton, HPos.CENTER);
         GridPane.setMargin(openButton, new Insets(20, 0, 20, 0));
+
+        fileButton.setOnAction(event -> {
+            File file = fileChooser.showOpenDialog(primaryStage);
+
+            if (file != null) {
+                for (Node child : gridPane.getChildren()) {
+                    if (child instanceof Label && gridPane.getColumnIndex(child) == 1 && gridPane.getRowIndex(child) == 2) {
+                        gridPane.getChildren().remove(child);
+                        Label newSelectedFileLabel = new Label(file.getAbsolutePath());
+                        newSelectedFileLabel.setStyle("-fx-font-weight: bold");
+                        gridPane.add(newSelectedFileLabel, 1, 2);
+
+                    } // TODO this seems to work fine, but throws 'ConcurrentModificationException'
+                }
+            }
+        });
 
         openButton.setOnAction(event -> {
             showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "TEST", "TEST Success");
